7bc35636625cae715e59717e7b73c774e0ceeacb
AlexV <asmodehn@gmail.com>
2014-05-06 11:44:41 +0900

    changed WinMain to standard main.

--- Tests/win32/main.cpp
+++ Tests/win32/main.cpp
@@ -4,15 +4,9 @@
 
 USING_NS_CC;
 
-int APIENTRY _tWinMain(HINSTANCE hInstance,
-                       HINSTANCE hPrevInstance,
-                       LPTSTR    lpCmdLine,
-                       int       nCmdShow)
+int main(int argc, char* argv[])
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // create the application instance
-    AppDelegate app;
-    return Application::getInstance()->run();
+	// create the application instance
+	AppDelegate app;
+	return Application::getInstance()->run();
 }
