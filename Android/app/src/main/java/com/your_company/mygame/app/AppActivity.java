12d38d40495ee0108389d22869945ac95a27b874
AlexV <asmodehn@gmail.com>
2014-07-31 18:41:06 +0900

    fixing android run.

--- Android/app/src/main/java/com/your_company/mygame/app/AppActivity.java
+++ Android/app/src/main/java/com/your_company/mygame/app/AppActivity.java
@@ -8,28 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxHandler;
+import org.cocos2dx.lib.Cocos2dxHelper;
+import org.cocos2dx.lib.Cocos2dxVideoHelper;
 
 public class AppActivity extends Cocos2dxActivity {
-
-    //we override this because we need to load 2 shared libraries
-    @Override
-    protected void onLoadNativeLibraries() {
-        try {
-            ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
-            Bundle bundle = ai.metaData;
-            try {
-                //String libName = bundle.getString("android.app.lib_name");
-                //System.loadLibrary(libName);
-                String appName = bundle.getString("android.app.app_name");
-                System.loadLibrary(appName);
-            } catch (Exception e) {
-                // ERROR
-                e.printStackTrace();
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            // ERROR
-            e.printStackTrace();
-        }
-    }
 
 }
