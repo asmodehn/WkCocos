65c8eb4fe33243d976c77de7e54c63e7221773d6
AlexV <asmodehn@gmail.com>
2014-08-06 16:00:01 +0900

    added test app license key in shop source.

--- Include/WkCocos/Shop/Shop.h
+++ Include/WkCocos/Shop/Shop.h
@@ -28,7 +28,8 @@ namespace WkCocos
 				soomla::CCServiceManager::getInstance()->setCommonParams(commonParams);
 
 				cocos2d::__Dictionary *storeParams = cocos2d::__Dictionary::create();
-				storeParams->setObject(cocos2d::__String::create("ExamplePublicKey"), "androidPublicKey");
+				//This key is in clear in code, and store in public repository, because this is not a real published app.
+				storeParams->setObject(cocos2d::__String::create("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArzKncfn7sGNNOg0p5Oz8zItUmaALQ7kivazs3WmdGXo9GVqzY6u3o2DylA9YuCCDeqLKklhhzTJbsd59cNFTQh/DN3rFA/7xn5AVb9Q6SopMYPHyqW0QGPS5cgXppGJ5I6tY6epBSuShwBXPmgAajbPF+/Viig11PDy4/iaqAWvvXG7TCwhEy2OklFH/VDwfES+rdH3nEd15SZ5xT4CstrnO+Dqcsi3iGa72Si5yWgzdCjzv0tHVR+WB5RwU1iYd4nl+qMF7GKlQVIE/AcTuOOh8eKOFQ2NP6eB42I1t3Sz8iXBo27C+D5cYMRqP2Gsy+8GVS8YgnO2BBSynXm6o7QIDAQAB"), "androidPublicKey");
 
 				MuffinRushAssets *assets = MuffinRushAssets::create();
 
