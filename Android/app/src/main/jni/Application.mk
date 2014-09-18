99fef289cdf6c8743c4b83b49b909bac7ddcce1a
AlexV <asmodehn@gmail.com>
2014-09-18 15:53:58 +0900

    fixing build by removing ABI ARM < v7

--- Android/app/src/main/jni/Application.mk
+++ Android/app/src/main/jni/Application.mk
@@ -7,7 +7,7 @@ NDK_TOOLCHAIN_VERSION := 4.8
 APP_PLATFORM := android-9
 
 #building for all platforms but our minimal supported ABI is ARMv7A
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi-v7a x86
 
 APP_STL := gnustl_static
 APP_CPPFLAGS :=  -std=c++11 -fsigned-char -frtti -fexceptions
