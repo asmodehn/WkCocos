b714b31d10ce613de4759ad7d13e59dfce9bc04f
bvs23bkv33 <bvs23bkv33@hotmail.com>
2014-09-19 10:21:40 +0300

    local PN without deprecated methods

--- Android/app/src/main/java/com/gameparkstudio/wkcocos/app/AppActivity.java
+++ Android/app/src/main/java/com/gameparkstudio/wkcocos/app/AppActivity.java
@@ -10,11 +10,10 @@ package com.gameparkstudio.wkcocos.app;
 import org.cocos2dx.lib.Cocos2dxActivity;
 
 import android.app.NotificationManager;
-import android.app.Notification;
 import android.app.PendingIntent;
-//import android.view.View;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 
 //import org.cocos2dx.lib.Cocos2dxHandler;
 //import org.cocos2dx.lib.Cocos2dxHelper;
@@ -42,19 +41,20 @@ public class AppActivity extends Cocos2dxActivity {
         return glSurfaceView;
     }
 
-    @SuppressWarnings("deprecation")
     @Override protected void onPause()
     {
         super.onPause();
         ServiceManager.getInstance().onPause();
         NM=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notify=new Notification(android.R.drawable.stat_notify_more,
-                "WkCocos notification", System.currentTimeMillis());
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(android.R.drawable.stat_notify_more) //what are available options to set here?
+                        .setContentTitle("WkCocos notification")
+                        .setContentText("Some notification text");
         PendingIntent pending=PendingIntent.getActivity(
                 getApplicationContext(), 0, new Intent(), 0);
-        notify.setLatestEventInfo(getApplicationContext(), "New notification",
-                "Some notification text", pending);
-        NM.notify(0, notify);
+        mBuilder.setContentIntent(pending);
+        NM.notify(0, mBuilder.build());
     }
 
     @Override protected void onResume() { ServiceManager.getInstance().onResume(); super.onResume(); }
