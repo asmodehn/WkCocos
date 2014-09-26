067978032f2e53e20165d50a385e796456a897b4
AlexV <asmodehn@gmail.com>
2014-09-26 18:10:34 +0900

    first version of PNManager, java side only.

--- Android/app/src/main/java/com/gameparkstudio/wkcocos/app/AppActivity.java
+++ Android/app/src/main/java/com/gameparkstudio/wkcocos/app/AppActivity.java
@@ -4,32 +4,38 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
+import com.gameparkstudio.wkcocos.lib.Cocos2dxWebViewHelper;
 import com.gameparkstudio.wkcocos.lib.MainActivity;
+import com.gameparkstudio.wkcocos.lib.PushNotificationsManager;
+import com.gameparkstudio.wkcocos.lib.Utils;
 
 public class AppActivity extends MainActivity {
 
     private final static String TAG = MainActivity.class.getSimpleName();
 
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //TODO : can we do that in MainActivity in lib ??
+        PushNotificationsManager.getInstance().setActivity(AppActivity.class);
+    }
+
     @Override protected void onPause()
     {
         super.onPause();
-        NotificationManager WKNM=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder WKBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(android.R.drawable.stat_notify_more) //what are available options to set here?
-                        .setContentTitle("WkCocos title")
-                        .setContentText("WkCocos text");
-        Intent WKIntent = new Intent(this, AppActivity.class);
-        PendingIntent WKPI=PendingIntent.getActivity(
-                getApplicationContext(), 0, WKIntent, 0);
-        WKBuilder.setContentIntent(WKPI);
-        WKNM.notify(0, WKBuilder.build());
+        //TODO : JNI binding to remove this from here and make it doable in C++ app
+        //TODO : FIX "when" doesnt seem to work ?
+        PushNotificationsManager.getInstance().Schedule(42, "Title Wkcocos", "UsefulMessage", R.drawable.ic_launcher);
     }
 
     @Override protected void onResume() {
         //Log.d(TAG, "Resuming app version " + Utils.getVersionName() );
         super.onResume();
+        //TODO : cancel remaining PN
     }
 }
---
+++ Android/lib/src/main/java/com/gameparkstudio/wkcocos/lib/PushNotificationsManager.java
@@ -0,0 +1,83 @@
+package com.gameparkstudio.wkcocos.lib;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+//This is a singleton. ref : http://howtodoinjava.com/2012/10/22/singleton-design-pattern-in-java
+//when used via getInstance(), it will retrieve the context from the main activity
+//This context can be used to launch other activities
+public class PushNotificationsManager{
+
+String TAG = "wkcocosPN";
+protected boolean mDebugEnabled = true;
+
+/*** Singleton design with MainActivity Context Access ***/
+	//managing serialization versions
+	private static final long serialVersionUID = 1L;
+
+	//clean singleton implementation
+	private static class LazyHolder
+	{
+		private static final PushNotificationsManager INSTANCE = new PushNotificationsManager();
+	}
+
+	private Context appContext;
+    private Class<?> appPNClickActivity;
+
+	private PushNotificationsManager()
+	{
+	    TAG += "/" + getClass().getSimpleName();
+		appContext = MainActivity.getContext();
+	}
+	
+	public static PushNotificationsManager getInstance()
+	{
+		return LazyHolder.INSTANCE;
+	}
+	
+	//this avoids issues with serialization
+	protected Object readResolve()
+	{
+		return getInstance();
+	}
+/*** ***/
+
+    public Boolean setActivity(Class pnClickActivity)
+    {
+        if ( Activity.class.isAssignableFrom(pnClickActivity)) {
+            appPNClickActivity = pnClickActivity;
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean Schedule(long when, String title, String message, int icon )
+    {
+        NotificationManager WKNM=(NotificationManager)appContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder WKBuilder =
+                new NotificationCompat.Builder(appContext)
+                        .setSmallIcon(icon)
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .setWhen(when);
+
+        Intent WKIntent = new Intent(appContext, appPNClickActivity );
+        PendingIntent WKPI=PendingIntent.getActivity(
+                appContext, 0, WKIntent, 0);
+        WKBuilder.setContentIntent(WKPI);
+        WKNM.notify(0, WKBuilder.build());
+
+        return true;
+    }
+	
+	protected void debugLog(String message) {
+        if (mDebugEnabled) {
+            Log.d(TAG, message);
+        }
+    }
+}
