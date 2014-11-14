7e6fb83fb53a458d2946b0290906ceadd617ce49
AlexV <asmodehn@gmail.com>
2014-11-14 15:26:44 +0900

    Fixing actor crash on exit when Actor is copied and id is duplicated.

--- Classes/Lib/Actor.cpp
+++ Classes/Lib/Actor.cpp
@@ -7,9 +7,28 @@ namespace WkCocos
 
 	std::default_random_engine Actor::generator;
 	std::uniform_int_distribution<ActorID> Actor::distribution;
-	
+
 	std::shared_ptr<entityx::EventManager> Actor::event_manager;
 	//
+
+	ActorID Actor::registerMe(Actor* act)
+	{
+	    //generate an id that is not in the list
+		ActorID id = distribution(generator);  // generates id
+
+		//tentative insert into actor map
+		bool inserted = false;
+		while (!inserted) // careful we have a max number of actors !!
+		{
+			std::pair<std::unordered_map<ActorID, Actor *>::iterator, bool> res = actors.insert(std::make_pair(id, act));
+			if (!res.second)// cannot be inserted -> regenerate id
+			{
+				id = distribution(generator);
+			}
+			inserted = res.second;
+		}
+		return id;
+	}
 
 	Actor::Actor()
 	{
@@ -19,25 +38,14 @@ namespace WkCocos
 			event_manager = entityx::EventManager::make();
 		}
 
-		//generate an id that is not in the list
-		ActorID id = distribution(generator);  // generates id
-
-		//tentative insert into actor map
-		bool inserted = false;
-		while (!inserted) // careful we have a max number of actors !!
-		{
-			std::pair<std::unordered_map<ActorID, Actor *>::iterator, bool> res = actors.insert(std::make_pair(id, this));
-			if (res.second) {
-				m_id = id;
-			}
-			else
-			{
-				id = distribution(generator);
-			}
-			inserted = res.second;
-		}
+        m_id = registerMe(this);
 	}
-	
+
+	Actor::Actor(const Actor& a)
+	{
+        m_id = registerMe(this);
+	}
+
 	Actor::~Actor()
 	{
 		//removing myself from actors list
@@ -63,5 +71,5 @@ namespace WkCocos
 		}
 		return nullptr;
 	}
-	
+
 }//namespace WkCocos
