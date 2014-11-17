fdaa29ed67bf9f481bc3b7389988d585ef5094f6
bvs23bkv33 <bvs23bkv33@hotmail.com>
2014-11-17 07:31:20 +0200

    added ignoring online data manager actions after timeout, fixed #61

--- Classes/Lib/OnlineData/Comp/OnlineData.cpp
+++ Classes/Lib/OnlineData/Comp/OnlineData.cpp
@@ -21,26 +21,29 @@ namespace WkCocos
 				, m_passwd(passwd)
 				, m_email(email)
 			{
-				m_cb = [=](void* data) {
-					::App42::App42UserResponse* userdata = static_cast<::App42::App42UserResponse*>(data);
-
-					CCLOG("\ncode=%d...=%d", userdata->getCode(), userdata->isSuccess);
-					CCLOG("\nResponse Body=%s", userdata->getBody().c_str());
-					
-					if (userdata->isSuccess)
-					{//if creation succeed then login.
-						cb(userdata);
-					}
-					else// if creation failed, 
+				m_cb = [=](void* data)
+				{
+					if (!timeout)
 					{
-						CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
-						CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
-						CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
-						CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
+						::App42::App42UserResponse* userdata = static_cast<::App42::App42UserResponse*>(data);
 
-						cb(userdata);
+						CCLOG("\ncode=%d...=%d", userdata->getCode(), userdata->isSuccess);
+						CCLOG("\nResponse Body=%s", userdata->getBody().c_str());
+
+						if (userdata->isSuccess)
+						{//if creation succeed then login.
+							cb(userdata);
+						}
+						else// if creation failed, 
+						{
+							CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
+							CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
+							CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
+							CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
+
+							cb(userdata);
+						}
 					}
-
 					done = true;
 					//userdata is deleted by App42SDK
 				};
@@ -54,26 +57,29 @@ namespace WkCocos
 				, m_userid(userid)
 				, m_passwd(passwd)
 			{
-				m_cb = [=](void* data) {
-					::App42::App42UserResponse* userdata = static_cast<::App42::App42UserResponse*>(data);
-
-					CCLOG("\ncode=%d...=%d", userdata->getCode(), userdata->isSuccess);
-					CCLOG("\nResponse Body=%s", userdata->getBody().c_str());
-
-					if (userdata->isSuccess)
-					{//if login succeed then login.
-						cb(userdata);
-					}
-					else// if login failed, 
+				m_cb = [=](void* data)
+				{
+					if (!timeout)
 					{
-						CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
-						CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
-						CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
-						CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
+						::App42::App42UserResponse* userdata = static_cast<::App42::App42UserResponse*>(data);
 
-						cb(userdata);
+						CCLOG("\ncode=%d...=%d", userdata->getCode(), userdata->isSuccess);
+						CCLOG("\nResponse Body=%s", userdata->getBody().c_str());
+
+						if (userdata->isSuccess)
+						{//if login succeed then login.
+							cb(userdata);
+						}
+						else// if login failed, 
+						{
+							CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
+							CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
+							CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
+							CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
+
+							cb(userdata);
+						}
 					}
-
 					done = true;
 					//userdata is deleted by App42SDK
 				};
@@ -91,17 +97,19 @@ namespace WkCocos
 			{
 				m_cb = [=](void* data)
 				{
-					::App42::App42StorageResponse* userdata = static_cast<::App42::App42StorageResponse*>(data);
+					if (!timeout)
+					{
+						::App42::App42StorageResponse* userdata = static_cast<::App42::App42StorageResponse*>(data);
 
-					CCLOG("\ncode=%d...=%d", userdata->getCode(), userdata->isSuccess);
+						CCLOG("\ncode=%d...=%d", userdata->getCode(), userdata->isSuccess);
 
-					CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
-					CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
-					CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
-					CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
+						CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
+						CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
+						CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
+						CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
 
-					cb(userdata);
-
+						cb(userdata);
+					}
 					done = true; 
 				};
 			}
@@ -117,17 +125,19 @@ namespace WkCocos
 			{
 				m_cb = [=](void* data)
 				{
-					::App42::App42StorageResponse* userdata = static_cast<::App42::App42StorageResponse*>(data);
+					if (!timeout)
+					{
+						::App42::App42StorageResponse* userdata = static_cast<::App42::App42StorageResponse*>(data);
 
-					CCLOG("\ncode=%d...=%d", userdata->getCode(), userdata->isSuccess);
+						CCLOG("\ncode=%d...=%d", userdata->getCode(), userdata->isSuccess);
 
-					CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
-					CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
-					CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
-					CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
+						CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
+						CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
+						CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
+						CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
 
-					cb(userdata);
-
+						cb(userdata);
+					}
 					done = true;
 				};
 			}
@@ -140,12 +150,15 @@ namespace WkCocos
 				, m_userid(userid)
 				, m_collection(collection)
 			{
-				m_cb = [=](void* data) {
-					::App42::App42UserResponse* userdata = static_cast<::App42::App42UserResponse*>(data);
-					CCLOG("\ncode=%d...=%d", userdata->getCode(), userdata->isSuccess);
+				m_cb = [=](void* data)
+				{
+					if (!timeout)
+					{
+						::App42::App42UserResponse* userdata = static_cast<::App42::App42UserResponse*>(data);
+						CCLOG("\ncode=%d...=%d", userdata->getCode(), userdata->isSuccess);
 
-					cb(userdata);
-
+						cb(userdata);
+					}
 					done = true;
 				};
 			}
@@ -161,26 +174,30 @@ namespace WkCocos
 				, m_quantity(quantity)
 				, m_offset(offset)
 			{
-				m_cb = [=](void* data) {
-					::App42::App42StorageResponse* userdata = static_cast<::App42::App42StorageResponse*>(data);
-					std::map<std::string, std::string> doc;
-					if (userdata->isSuccess)
+				m_cb = [=](void* data)
+				{
+					if (!timeout)
 					{
-						auto firstStorage = userdata->storages.begin();
-						auto jsonDocArray = firstStorage->jsonDocArray;
-						for (std::vector<::App42::JSONDocument>::iterator it = jsonDocArray.begin(); it != jsonDocArray.end(); ++it)
+						::App42::App42StorageResponse* userdata = static_cast<::App42::App42StorageResponse*>(data);
+						std::map<std::string, std::string> doc;
+						if (userdata->isSuccess)
 						{
-							doc[it->getOwner()] = it->getJsonDoc();
+							auto firstStorage = userdata->storages.begin();
+							auto jsonDocArray = firstStorage->jsonDocArray;
+							for (std::vector<::App42::JSONDocument>::iterator it = jsonDocArray.begin(); it != jsonDocArray.end(); ++it)
+							{
+								doc[it->getOwner()] = it->getJsonDoc();
+							}
+							cb(doc, (int)firstStorage->recordCount);
 						}
-						cb(doc, (int)firstStorage->recordCount);
-					}
-					else
-					{
-						CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
-						CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
-						CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
-						CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
-						cb(doc, 0);
+						else
+						{
+							CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
+							CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
+							CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
+							CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
+							cb(doc, 0);
+						}
 					}
 					done = true;
 				};
@@ -198,32 +215,36 @@ namespace WkCocos
 				, m_quantity(quantity)
 				, m_offset(offset)
 			{
-				m_cb = [=](void* data) {
-					::App42::App42StorageResponse* userdata = static_cast<::App42::App42StorageResponse*>(data);
-					std::map<std::string, std::string> doc;
-					if (userdata->isSuccess)
-					{	
-						auto firstStorage = userdata->storages.begin();
-						auto jsonDocArray = firstStorage->jsonDocArray;
-						for (std::vector<::App42::JSONDocument>::iterator it = jsonDocArray.begin(); it != jsonDocArray.end(); ++it)
+				m_cb = [=](void* data)
+				{
+					if (!timeout)
+					{
+						::App42::App42StorageResponse* userdata = static_cast<::App42::App42StorageResponse*>(data);
+						std::map<std::string, std::string> doc;
+						if (userdata->isSuccess)
 						{
-							doc[it->getOwner()] = it->getJsonDoc();
+							auto firstStorage = userdata->storages.begin();
+							auto jsonDocArray = firstStorage->jsonDocArray;
+							for (std::vector<::App42::JSONDocument>::iterator it = jsonDocArray.begin(); it != jsonDocArray.end(); ++it)
+							{
+								doc[it->getOwner()] = it->getJsonDoc();
+							}
+							cb(doc, (int)firstStorage->recordCount);
 						}
-						cb(doc, (int)firstStorage->recordCount);
-					}
-					else if (404  == userdata->httpErrorCode && 2608 == userdata->appErrorCode)
-					{
-						// emulate no user found
-						// game logic needs to know when request failed.
-						cb(doc, 0);
-					}
-					else// if request failed,
-					{
-						CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
-						CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
-						CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
-						CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
-						cb(doc, 0);
+						else if (404 == userdata->httpErrorCode && 2608 == userdata->appErrorCode)
+						{
+							// emulate no user found
+							// game logic needs to know when request failed.
+							cb(doc, 0);
+						}
+						else// if request failed,
+						{
+							CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
+							CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
+							CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
+							CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
+							cb(doc, 0);
+						}
 					}
 					done = true;
 				};
@@ -237,14 +258,16 @@ namespace WkCocos
 			{
 				m_cb = [=](void* data)
 				{
-					::App42::App42TimerResponse* userdata = static_cast<::App42::App42TimerResponse*>(data);
-
-					if (userdata->isSuccess)
+					if (!timeout)
 					{
-						//event_emitter->emit<Events::ServerTime>(userdata->app42Timer.currentTime);
-						callback(userdata->app42Timer.currentTime);
-					}
+						::App42::App42TimerResponse* userdata = static_cast<::App42::App42TimerResponse*>(data);
 
+						if (userdata->isSuccess)
+						{
+							//event_emitter->emit<Events::ServerTime>(userdata->app42Timer.currentTime);
+							callback(userdata->app42Timer.currentTime);
+						}
+					}
 					done = true;
 				};
 			}
@@ -258,32 +281,36 @@ namespace WkCocos
 				, m_quantity(quantity)
 				, m_offset(offset)
 			{
-				m_cb = [=](void* data) {
-					::App42::App42StorageResponse* userdata = static_cast<::App42::App42StorageResponse*>(data);
-					std::vector<std::map<std::string, std::string>> table;
-					if (userdata->isSuccess)
+				m_cb = [=](void* data)
+				{
+					if (!timeout)
 					{
-						std::map<std::string, std::string> line;
-						auto jsonDocArray = userdata->storages.begin()->jsonDocArray;
-						for (std::vector<::App42::JSONDocument>::iterator it = jsonDocArray.begin(); it != jsonDocArray.end(); ++it)
+						::App42::App42StorageResponse* userdata = static_cast<::App42::App42StorageResponse*>(data);
+						std::vector<std::map<std::string, std::string>> table;
+						if (userdata->isSuccess)
 						{
-							line["Document_ID"] = it->getDocId();
-							line["JSON_Document"] = it->getJsonDoc();
-							line["Owner"] = it->getOwner();
-							line["Created_On"] = it->getCreatedAt();
-							line["Updated_On"] = it->getUpdatedAt();
-							table.push_back(line);
-						}
+							std::map<std::string, std::string> line;
+							auto jsonDocArray = userdata->storages.begin()->jsonDocArray;
+							for (std::vector<::App42::JSONDocument>::iterator it = jsonDocArray.begin(); it != jsonDocArray.end(); ++it)
+							{
+								line["Document_ID"] = it->getDocId();
+								line["JSON_Document"] = it->getJsonDoc();
+								line["Owner"] = it->getOwner();
+								line["Created_On"] = it->getCreatedAt();
+								line["Updated_On"] = it->getUpdatedAt();
+								table.push_back(line);
+							}
 
-						cb(table);
-					}
-					else
-					{
-						CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
-						CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
-						CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
-						CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
-						cb(table);
+							cb(table);
+						}
+						else
+						{
+							CCLOG("\nerrordetails:%s", userdata->errorDetails.c_str());
+							CCLOG("\nerrorMessage:%s", userdata->errorMessage.c_str());
+							CCLOG("\nappErrorCode:%d", userdata->appErrorCode);
+							CCLOG("\nhttpErrorCode:%d", userdata->httpErrorCode);
+							cb(table);
+						}
 					}
 					done = true;
 				};
