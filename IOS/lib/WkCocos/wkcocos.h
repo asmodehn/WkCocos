fbfe57afa027b6be26e0e53a9995a08647aea67d
julienm <julienm@julienms-Mac-mini.local>
2015-02-17 16:46:21 +0900

    add xcode for IOS version, WIP.

---
+++ IOS/lib/WkCocos/wkcocos.h
@@ -0,0 +1,13 @@
+//
+//  wkcocos.h
+//  wkcocos
+//
+//  Created by julienm on 2/17/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface wkcocos : NSObject
+
+@end
---
+++ IOS/lib/WkCocos/wkcocos.m
@@ -0,0 +1,13 @@
+//
+//  wkcocos.m
+//  wkcocos
+//
+//  Created by julienm on 2/17/15.
+//
+//
+
+#import "wkcocos.h"
+
+@implementation wkcocos
+
+@end
