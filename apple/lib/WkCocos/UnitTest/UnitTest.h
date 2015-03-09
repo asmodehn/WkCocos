7ffe89c5532e14579799f4f738ea6064a67885c1
Seunghwan <sparkishy@gmail.com>
2015-03-09 17:18:04 +0900

    game compilation

---
+++ apple/lib/WkCocos/UnitTest/UnitTest.h
@@ -0,0 +1,13 @@
+//
+//  UnitTest.h
+//  UnitTest
+//
+//  Created by seunghwanpark on 2015-03-06.
+//  Copyright (c) 2015 seunghwanpark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UnitTest : NSObject
+
+@end
---
+++ apple/lib/WkCocos/UnitTest/UnitTest.m
@@ -0,0 +1,13 @@
+//
+//  UnitTest.m
+//  UnitTest
+//
+//  Created by seunghwanpark on 2015-03-06.
+//  Copyright (c) 2015 seunghwanpark. All rights reserved.
+//
+
+#import "UnitTest.h"
+
+@implementation UnitTest
+
+@end
